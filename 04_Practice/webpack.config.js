01d6004ec856982240eb2d1155bd2395e8a7e999
Manjuanand <mtoladinni@tradeking.com>
2016-10-25 22:10:55 -0400

    use webpack-dev-server

--- 04_Practice/webpack.config.js
+++ 04_Practice/webpack.config.js
@@ -7,15 +7,13 @@
      },
      module: {
          loaders: [{
-                 test: /\.js$/,
-                 exclude: /node_modules/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             },
-             { test: /\.css$/, loader: "style-loader!css-loader" }
-         ]
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader',
+             query: {
+                 presets: ['es2015']
+             }
+         }]
      },
      plugins: [
          new webpack.optimize.UglifyJsPlugin({
